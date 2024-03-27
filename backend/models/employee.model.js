55840ba84c3c4e6cc6c8f07e7901a2b952bea3eb
imlnr <imlnr2002@gmail.com>
2024-03-27 16:21:32 +0530

    final commit

---
+++ backend/models/employee.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const EmployeeSchema = mongoose.Schema({
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true },
+    department: { type: String, required: true },
+    salary: { type: Number, required: true }
+}, {
+    versionKey: false
+})
+
+const EmployeeModel = mongoose.model("User", EmployeeSchema);
+
+module.exports = {
+    EmployeeModel
+}
---
+++ backend/routes/employee.routes.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const { EmployeeModel } = require('../models/employee.model');
+const employeeRouter = express.Router();
+
+
+employeeRouter.get('/employees', async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1; 
+        const limit = 5; 
+
+        const skip = (page - 1) * limit;
+
+        let filter = {};
+        if (req.query.department) {
+            filter.department = req.query.department;
+        }
+
+        let sort = {};
+        if (req.query.sortBy === 'salary') {
+            sort.salary = req.query.sortOrder === 'desc' ? -1 : 1;
+        }
+
+        const employees = await EmployeeModel.find(filter)
+            .sort(sort)
+            .skip(skip)
+            .limit(limit);
+
+        const totalCount = await EmployeeModel.countDocuments(filter);
+
+        const totalPages = Math.ceil(totalCount / limit);
+
+        res.status(200).json({
+            employees,
+            totalPages,
+            currentPage: page,
+            totalCount
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+employeeRouter.get('/employees/departments', async (req, res) => {
+    try {
+        const departments = await EmployeeModel.distinct('department');
+
+        res.status(200).json({ departments });
+    } catch (error) {
+
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+employeeRouter.get('/employees/search', async (req, res) => {
+    try {
+        const firstName = req.query.firstName;
+        const employees = await EmployeeModel.find({ firstName: { $regex: new RegExp(firstName, 'i') } });
+        res.status(200).json({ employees });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+employeeRouter.delete('/employee/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        await EmployeeModel.findByIdAndDelete({ _id: id });
+        res.status(202).send({ "msg": "deleted Successfully.." });
+    } catch (error) {
+        res.status(500).send({ "msg": "internal Server Error", error })
+    }
+})
+
+employeeRouter.put('/employee/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const { firstname, lastname, email, department, salary } = req.body;
+        const updateemployee = await EmployeeModel.findByIdAndUpdate(id, { firstname, lastname, email, department, salary }, { new: true });
+        if (!updateemployee) {
+            return res.status(404).send({ "msg": "employee not found!" })
+        }
+        res.status(204).send({ "msg": "updated successfully" });
+    } catch (error) {
+        res.status(400).json({ msg: error.message });
+    }
+})
+
+module.exports = employeeRouter;
