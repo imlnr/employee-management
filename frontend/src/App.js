1782c707e69ff1cc787297de9174427b994774dd
imlnr <imlnr2002@gmail.com>
2024-03-27 15:34:49 +0530

    react-basic-structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter } from 'react-router-dom'
+import MainRoutes from './allroutes/MainRoutes';
+import { ChakraProvider } from "@chakra-ui/react"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ChakraProvider>
+        <div className="App">
+          <MainRoutes />
+        </div>
+      </ChakraProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/allroutes/MainRoutes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+import Home from '../pages/Home'
+import Dashboard from '../pages/Dashboard'
+
+const MainRoutes = () => {
+    return (
+        <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/Signup' element={<Signup />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/' element={<Home />} />
+        </Routes>
+    )
+}
+
+export default MainRoutes
