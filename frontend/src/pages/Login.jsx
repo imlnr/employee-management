f469766aa6681b627c4977ebc0cda26469942edd
imlnr <imlnr2002@gmail.com>
2024-03-27 16:44:20 +0530

    final commit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,112 @@
-import React from 'react'
+import { useState } from "react";
+import {
+    Flex,
+    Heading,
+    Input,
+    Button,
+    InputGroup,
+    Stack,
+    InputLeftElement,
+    chakra,
+    Box,
+    Avatar,
+    FormControl,
+    FormHelperText,
+    InputRightElement
+} from "@chakra-ui/react";
+import { FaUserAlt, FaLock } from "react-icons/fa";
+import { Link as RouterLink } from "react-router-dom";
+const CFaUserAlt = chakra(FaUserAlt);
+const CFaLock = chakra(FaLock);
 
 const Login = () => {
-    return (
-        <div>Login</div>
-    )
-}
+    const [showPassword, setShowPassword] = useState(false);
 
-export default Login
+    const handleShowClick = () => setShowPassword(!showPassword);
+    const [email, setemail] = useState('');
+    const [password, setpassword] = useState('');
+
+    const handlesubmit = () => {
+
+    }
+
+    return (
+        <Flex
+            flexDirection="column"
+            width="100wh"
+            height="100vh"
+            backgroundColor="gray.200"
+            justifyContent="center"
+            alignItems="center"
+        >
+            <Stack
+                flexDir="column"
+                mb="2"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Avatar bg="teal.500" />
+                <Heading color="teal.400">Welcome</Heading>
+                <Box minW={{ base: "90%", md: "468px" }}>
+                    <form>
+                        <Stack
+                            spacing={4}
+                            p="1rem"
+                            backgroundColor="whiteAlpha.900"
+                            boxShadow="md"
+                        >
+                            <FormControl>
+                                <InputGroup>
+                                    <InputLeftElement
+                                        pointerEvents="none"
+                                        children={<CFaUserAlt color="gray.300" />}
+                                    />
+                                    <Input onChange={(e) => setemail(e.target.value)} type="email" placeholder="email address" />
+                                </InputGroup>
+                            </FormControl>
+                            <FormControl>
+                                <InputGroup>
+                                    <InputLeftElement
+                                        pointerEvents="none"
+                                        color="gray.300"
+                                        children={<CFaLock color="gray.300" />}
+                                    />
+                                    <Input onChange={(e) => setpassword(e.target.value)}
+                                        type={showPassword ? "text" : "password"}
+                                        placeholder="Password"
+                                    />
+                                    <InputRightElement width="4.5rem">
+                                        <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                                            {showPassword ? "Hide" : "Show"}
+                                        </Button>
+                                    </InputRightElement>
+                                </InputGroup>
+                                <FormHelperText textAlign="right">
+                                    <RouterLink>forgot password?</RouterLink>
+                                </FormHelperText>
+                            </FormControl>
+                            <Button
+                                borderRadius={0}
+                                type="submit"
+                                variant="solid"
+                                colorScheme="teal"
+                                width="full"
+                            >
+                                Login
+                            </Button>
+                        </Stack>
+                    </form>
+                </Box>
+            </Stack>
+            <Box>
+                New to us?{" "}
+                <RouterLink to="/signup" style={{ color: "#319795" }}>
+                    Sign Up
+                </RouterLink>
+
+            </Box>
+        </Flex>
+    );
+};
+
+export default Login;
